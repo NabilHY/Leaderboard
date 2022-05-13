4d8f00035e484e2d283356f9815fc6eea46400fb
nabsternab <hayounn.etd@gmail.com>
2022-05-13 13:08:42 +0100

    Implement function to post and get data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
 import './style.css';
+import leaderboard from './addPlayer.js';
+
+// DOM Selectors
+const refreshBtn = document.getElementById('refresh');
+const leaderBoard = document.querySelector('ul');
+const submit = document.getElementById('submit');
+const score = document.getElementById('score');
+const player = document.getElementById('player');
+
+// Click event to post to api
+submit.addEventListener('click', async (e) => {
+  e.preventDefault();
+  const scoreType = parseInt(score.value, 10);
+  if (scoreType && player.value !== '' && scoreType !== '') {
+    await leaderboard.createScore(player.value, score.value);
+    player.value = '';
+    score.value = '';
+  }
+});
+
+// click event to get from api
+refreshBtn.addEventListener('click', async () => {
+  const scores = await leaderboard.getScores();
+  const res = scores.result;
+  leaderBoard.innerHTML = '';
+  for (let i = 0; i < res.length; i += 1) {
+    leaderBoard.innerHTML += `
+        <li>${res[i].user}: ${res[i].score}</li>
+        `;
+  }
+});
